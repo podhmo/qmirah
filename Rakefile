f52c2d7f7f30fe6ee39c06b7c952ee8c39eba637
podhmo <ababjam61@gmail.com>
2010-09-20 21:07:14 +0900

    -

--- Rakefile
+++ Rakefile
@@ -1,3 +1,22 @@
+
+desc "install"
+task :install do
+  require 'erb'
+  require 'yaml'
+  CONF = YAML.load_file("setting.yaml")
+
+  make_file = lambda do |name,command|
+    CONF["command"] = command
+    File.open(name, "w") do |wf|
+      wf.puts ERB.new(File.read("qmirah.erb")).result
+    end
+    File.chmod(0764, name)
+  end
+  
+  make_file.call("qmirah","run")
+  make_file.call("qmirahc","compile")
+end
+
 task :clean do
   sh "rm Fib*"
 end
--- mirah_server.rb
+++ mirah_server.rb
@@ -6,23 +6,41 @@ rescue LoadError
   require 'mirah'
 end
 
-port = ARGV.first || "44444"
+## monkey patching
+class DubyImpl
+  def exit(n)
+    raise "exit with status #{n}"
+  end
+end
+
 duby = DubyImpl.new
 
-TCPServer.open('localhost',port) do |s|
-  STDERR.puts "Listening with #{port} ..."
-  is_looping = true
-  while is_looping
-    cl = s.accept
-    Thread.start {
-      data = cl.gets.chomp
-      is_looping = false if data == "end"
-      STDERR.puts "receive data: #{data}"
-      cl.puts "receive args: #{data}"
-      cl.close
-      duby.compile(*data.split(" "))
-    }
+def duby.doaction(str)
+  cmd, *args = str.split(" ")
+  begin
+    send(cmd.to_sym, *args)
+  rescue NoMethodError
+    send(:print_help)
   end
-  Thread.list.each{|t| Thread.kill(t) unless t == Thread.current}
-  puts "...End"
 end
+
+require 'socket'
+def with_server(name,port)
+  TCPServer.open(name,port) do |s|
+    STDERR.puts "Listening with #{port} ..."
+    yield(s)
+    Thread.list.each{|t| Thread.kill(t) unless t == Thread.current}
+    STDERR.puts "...End"
+  end
+end
+
+with_server('localhost', "44444") do |s|
+  loop do
+    Thread.start(s.accept) do |cl|
+      duby.doaction cl.gets.chomp.tap{|e| STDERR.puts "recv: #{e}"}
+      cl.close
+    end
+  end
+end
+
+
